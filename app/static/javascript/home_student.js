bc9d4c1da622cc5219b9120076083687cc844ff1
DaniloCaro <94326335+DaniloCaro@users.noreply.github.com>
2022-02-08 17:19:46 -0500

    Se modificó estilos de home student

--- app/static/javascript/home_student.js
+++ app/static/javascript/home_student.js
@@ -37,3 +37,15 @@ function ValidateSingleInput(oInput) {
     }
     return true;
 }
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (navMenu.classList.contains("nav-menu_visible")){
+        navToggle.setAttribute("arial-label", "Cerrar menú");
+    } else {
+        navToggle.setAttribute("arial-label", "Abrir menú");
+    }
+});
