bc888a8144bbff2c8ca6db4fce306ac664fd6230
Softpymes Roberth <sebuscaroberth@gmail.com>
2022-02-12 16:37:48 -0500

    implementando manejo de sesiones y optimizando codigo para las funcionalidades del adm

--- app/routes/Administrativo.py
+++ app/routes/Administrativo.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from models.models import Student, Administrative
 from services.TypeId import typeIdService
 from services.Documents import documentService
 
 Administrativo = Blueprint('Administrativo', __name__)
 
-@Administrativo.route('/home_administrativo')
+@Administrativo.route('/home_administrativo',)
 def home_administrativo():
-    typeids = typeIdService().get_typeIds()
-    alldocuments = documentService().get_documents()
-    return render_template('home_administrative.html', typeids=typeids, documents=alldocuments)
+    if "code" in session:
+        code = session["code"]
+        administrative = Administrative.query.filter_by(administrative_code=code).first()
+        typeids = typeIdService().get_typeIds()
+        alldocuments = documentService().get_documents()
+        return render_template("home_administrative.html",administrative=administrative,typeids=typeids, documents=alldocuments)
+    else:
+        return redirect(url_for("auths.index"))
