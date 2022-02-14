1be3fc827da6006d3e69e1d47da8bcac8b699be9
DaniloCaro <94326335+DaniloCaro@users.noreply.github.com>
2022-02-13 19:21:13 -0500

    13022022 correcion de estilos en main admin

--- app/static/javascript/home_adm.js
+++ app/static/javascript/home_adm.js
@@ -6,3 +6,25 @@ function cerrar(){
 if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
 }
+function search() {
+var tabla = document.getElementById("tablaDocument");
+var busqueda = document.getElementById("inputSearch").value.toLowerCase();
+var cellsOfRow = "";
+var found = false;
+var compareWith = "";
+for (var i = 1; i < tabla.rows.length; i++) {
+    cellsOfRow = tabla.rows[i].getElementsByTagName("td");
+    found = false;
+    for (var j = 0; j < cellsOfRow.length && !found; j++) {
+        compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+    if (busqueda.length == 0 || compareWith.indexOf(busqueda) > -1) {
+        found = true;
+    }
+    }
+    if (found) {
+        tabla.rows[i].style.display = "";
+    } else {
+        tabla.rows[i].style.display = "none";
+    }
+    }
+}
