2a4724ca22f80bbe349ba9e6e40a488f21d01c3c
Softpymes Roberth <sebuscaroberth@gmail.com>
2022-02-16 14:29:30 -0500

    cambios danilo

--- query_typeId.sql
+++ query_typeId.sql
@@ -28,18 +28,9 @@ VALUES('En revision'),('Rechazado'),('Aceptado')
 
 --querys para postgrest
 
-INSERT INTO public."typeId"(description)
-VALUES('Cedula de Ciudadania'),('Cedula de Extranjeria'),('Certificado de Cabildo'),(
-    'Documento de identidad Extranjera'
-),('Nit'),('Pasaporte'),('Tarjeta de identidad')
+INSERT INTO public."typeId"(description)VALUES('Cedula de Ciudadania'),('Cedula de Extranjeria'),('Certificado de Cabildo'),('Documento de identidad Extranjera'),('Nit'),('Pasaporte'),('Tarjeta de identidad')
 
-INSERT INTO public."Administrative"(
-	administrative_code, name, lastname, id_typeid, "IDNumber")
-	VALUES ('7541713001',
-    'Julanito',
-    'Perez',
-    '1',
-    '110265986');
+INSERT INTO public."Administrative"(administrative_code, name, lastname, id_typeid, "IDNumber")VALUES ('7541713001','Julanito','Perez','1','110265986');
 
 INSERT INTO public."State"( description)
 	VALUES ('En revision'),('Rechazado'),('Aceptado');
